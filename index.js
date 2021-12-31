a990ce399e926a3d5810f47e7b0bda84fbc13207
laggingreflex <laggingreflex@gmail.com>
2021-12-31 11:34:25 +0530

    jsdoc

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import { join } from 'path';
 import { readdir } from 'fs/promises';
+import { Dirent } from 'fs';
 
-export default async function* readdirPAsync(parent, opts, state) {
-  const children = await readdir(parent, { withFileTypes: true });
+/**
+ * readdirp async generator
+ * @async
+ * @param {string} path
+ * @param {object} [opts]
+ * @param {number} [opts.depth]
+ * @yields {output}
+ */
+export default async function* readdirPAsync(path, opts, state) {
+  const children = await readdir(path, { withFileTypes: true });
   for (const dirent of children) {
-    const child = join(parent, dirent.name);
-    const output = { path: child, parent, dirent, depth: state?.depth };
+    const child = join(path, dirent.name);
+    /** @type {output} */
+    const output = { path: child, parent: path, dirent, depth: state?.depth };
+    /** @type {input} */
     const input = Object.assign(output, yield output);
     if (input.skip) continue;
     if (!dirent.isDirectory()) continue;
@@ -13,3 +24,16 @@ export default async function* readdirPAsync(parent, opts, state) {
     yield* readdirPAsync(child, opts, { ...state, depth: (state?.depth ?? 0) + 1 });
   }
 }
+
+/**
+ * @typedef {object} output
+ * @property {string} path
+ * @property {string} parent
+ * @property {Dirent} dirent
+ * @property {number} depth
+ */
+
+/**
+ * @typedef {object} input
+ * @property {boolean} [skip]
+ */
