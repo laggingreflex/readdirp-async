3524d490fd8c43ae665af409d01d504e93d835b4
laggingreflex <laggingreflex@gmail.com>
2021-12-31 18:03:06 +0530

    fix: try deeper symbolink dirs

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
+import util from 'util';
 import { join } from 'path';
 import { readdir } from 'fs/promises';
 import { Dirent } from 'fs';
+const debug = util.debug('readdirp-async');
 
 /**
  * readdirp async generator
  * @async
  * @param {string} path
  * @param {object} [opts]
- * @param {number} [opts.depth]
+ * @param {number} [opts.depth=Infinity]
+ * @param {boolean} [opts.follow=true] Follow links
+ * @param {boolean} [opts.halt=false] Halt at errors reading descendant directories
+ * @param {boolean} [opts.silent=true] Don't log or warn about errors & successes
  * @yields {output}
  */
-export default async function* readdirPAsync(path, opts, state) {
+export default async function* readdirPAsync(path, opts, { depth = 0, state = { size: 0, errors: 0 } } = {}) {
   const children = await readdir(path, { withFileTypes: true });
   for (const dirent of children) {
+    state.size++;
     const child = join(path, dirent.name);
     /** @type {output} */
-    const output = { path: child, parent: path, dirent, depth: state?.depth };
+    const output = { path: child, parent: path, dirent, depth, ...state };
     /** @type {input} */
     const input = Object.assign(output, yield output);
+    debug({ path, child, output, input });
     if (input.skip) continue;
-    if (!dirent.isDirectory()) continue;
-    if ((state?.depth ?? 0) >= (opts?.depth ?? Infinity)) continue;
-    yield* readdirPAsync(child, opts, { ...state, depth: (state?.depth ?? 0) + 1 });
+    if (depth >= (opts?.depth ?? Infinity)) continue;
+    if (!dirent.isDirectory() && (!dirent.isSymbolicLink() || !opts?.follow)) continue;
+    try {
+      yield* readdirPAsync(child, opts, { depth: depth + 1, state });
+    } catch (error) {
+      if (opts.halt) throw error;
+      state.errors++;
+      debug(error);
+    }
+  }
+
+  if (opts.silent === false && depth === 0) {
+    if (state.errors) {
+      console.warn(`${state.errors}/${state.size} descendants couldn't be read from: ${path}`);
+    } else {
+      console.log(`${state.size} descendants read from: ${path}`);
+    }
   }
 }
 
@@ -31,6 +52,8 @@ export default async function* readdirPAsync(path, opts, state) {
  * @property {string} parent
  * @property {Dirent} dirent
  * @property {number} depth
+ * @property {number} size
+ * @property {number} errors
  */
 
 /**
